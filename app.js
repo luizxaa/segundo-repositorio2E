889011fd471cd545a34cce3384f0f8b0dfccb152
Gerson Cesar Grobe de Miranda <106871453+cesargrobe@users.noreply.github.com>
2024-11-06 21:56:52 -0300

    Aula 09 - 06/11

--- app.js
+++ app.js
@@ -20,6 +20,16 @@ function criaCartao (item, pergunta, resposta) {
             </div>
         </div>
     `
+
+    let respostaEstaVisivel = false;
+
+    function viraCartao () {
+        respostaEstaVisivel = !variavelEstaVisivel;
+        cartao.classList.toggle('active', viraCartao);
+    }
+
+    cartao.addEventListener('click', viraCartao);
+
     flashcard.appendChield(cartao);
 
 }
