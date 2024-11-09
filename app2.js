b2fb4630f685972d272759d03bf04b1bc9776ca7
Gerson Cesar Grobe de Miranda <106871453+cesargrobe@users.noreply.github.com>
2024-11-09 16:58:33 -0300

    2 19

--- app2.js
+++ app2.js
@@ -1,3 +1,23 @@
 function criaCartao(item, pergunta, resposta) {
-    console.log(item, pergunta, resposta)
+    //console.log(item, pergunta, resposta)
+    let flashcard = document.getElementById('flashcard')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+
+    cartao.innerHTML = `
+        <div class="cartao__conteudo">
+            <h3>${item}</h3>
+            
+            <div class="cartao__conteudo__pergunta">
+                <p>${pergunta}</p>
+            </div>
+            
+            <div class="cartao__conteudo__resposta">
+                <p>${resposta}</p>
+            </div>
+        </div>
+    `
+
+    flashcard.appendChild(cartao)
+
 }
