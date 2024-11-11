c2177b8e86a6b8ac5191119316784e9be54c872d
Gerson Cesar Grobe de Miranda <106871453+cesargrobe@users.noreply.github.com>
2024-11-11 11:05:42 -0300

    testes10

---
+++ favorito.js
@@ -0,0 +1,16 @@
+
+
+document.getElementById('favorito').addEventListener('click', function() {
+    const imgElement = this; 
+    fetch(imgElement.src) 
+        .then(response => response.text()) 
+        .then(data => { 
+            const parser = new DOMParser();
+            const svgDoc = parser.parseFromString(data, "image/svg+xml"); 
+            const svgElement = svgDoc.querySelector('svg'); 
+            const currentColor = svgElement.getAttribute('fill'); svgElement.setAttribute('fill', currentColor === 'white' ? 'red' : 'white'); 
+            const serializedSvg = new XMLSerializer().serializeToString(svgElement); 
+            const svgBlob = new Blob([serializedSvg], { type: 'image/svg+xml' }); 
+            const url = URL.createObjectURL(svgBlob); imgElement.src = url; 
+        }); 
+}); 
