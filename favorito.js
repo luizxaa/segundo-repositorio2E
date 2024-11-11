588899fc0b7bfccb911d208be8d91becd558b315
Gerson Cesar Grobe de Miranda <106871453+cesargrobe@users.noreply.github.com>
2024-11-11 11:30:55 -0300

    testes16

--- favorito.js
+++ favorito.js
@@ -1,21 +1,25 @@
 
 
-document.getElementById('favorito').addEventListener('click', function() {
-    const imgElement = this; 
+function toggleFavorite(imgElement) {
     fetch(imgElement.src) 
         .then(response => response.text()) 
         .then(data => { 
-            const parser = new DOMParser();
-            const svgDoc = parser.parseFromString(data, "image/svg+xml");
-            const svgElement = svgDoc.querySelector('path'); // Alterado para selecionar o elemento <path>
-            const currentColor = svgElement.getAttribute('fill');
-            svgElement.setAttribute('fill', currentColor === 'white' ? 'red' : 'white'); // Alterado para 'white' 
-            const serializedSvg = new XMLSerializer().serializeToString(svgElement.ownerDocument.documentElement);
-            const svgBlob = new Blob([serializedSvg], { type: 'image/svg+xml' });
-            const url = URL.createObjectURL(svgBlob);
+            const parser = new DOMParser(); 
+            const svgDoc = parser.parseFromString(data, "image/svg+xml"); 
+            const svgElement = svgDoc.querySelector('path'); // Seleciona o elemento <path> 
+            const currentColor = svgElement.getAttribute('fill'); svgElement.setAttribute('fill', currentColor === 'white' ? 'red' : 'white'); // Alterna a cor 
+            const serializedSvg = new XMLSerializer().serializeToString(svgElement.ownerDocument.documentElement); 
+            const svgBlob = new Blob([serializedSvg], { type: 'image/svg+xml' }); 
+            const url = URL.createObjectURL(svgBlob); 
             imgElement.src = url;
-
+            
             // Aumenta a imagem em 5% 
-            imgElement.style.transform = imgElement.style.transform === 'scale(1.1)' ? 'scale(1)' : 'scale(1.1)';
-        }); 
-}); 
+            imgElement.style.transform = imgElement.style.transform === 'scale(1.05)' ? 'scale(1)' : 'scale(1.05)'; 
+    }); 
+} 
+    
+document.querySelectorAll('img[src="./img/favoritos.svg"]').forEach(img => { 
+    img.addEventListener('click', function() { 
+        toggleFavorite(this); 
+    }); 
+});
